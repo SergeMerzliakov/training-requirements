14d05849dca77295bb6b90e1543759f7eccee9aa
Serge Merzliakov <smerzlia@gmail.com>
2021-05-25 21:18:44 +1000

    Added empty Q3 R script

---
+++ R for Data Science/src/gapMinder_Analysis_Q3.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(tidyverse)
+library(scales)
+library(plotly)
+
+# Q3 - What is the country (or countries) that has the highest 'Population density (people per sq. km of land area)'
+# across all years? (i.e., which country has the highest average ranking in this category across each time
+# point in the dataset?)
+
+graphics.off()
+rm(list=ls())
+setwd("~/dev/training-requirements/R for Data Science")
+gapminder <- as.tibble(read_csv("gapminder_clean.csv"))
+names(gapminder)[2]<-"country"
+names(gapminder)[8]<-"energyUse"
+names(gapminder)[12]<-"imports"
+
+
+
+
+
+
+
+
+
